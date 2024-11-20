f33217060d81a09953ced9ed66cd6c40b6088106
kimbleex <kimbleex@outlook.com>
2024-11-20 13:50:06 +0800

    png -> avif

--- convert.py
+++ convert.py
@@ -13,6 +13,6 @@ import pillow_avif       # pillow-avif-plugin        1.2.2
 # for i in os.listdir(path[0]):
     
 # path_ = path[0] + i
-path_ = "./BlogIMG/AVIF_Pillow/vis_compare.png"
+path_ = r"./Cover_unused/蜡笔小新55.png"
 img = Image.open(path_)
 img.save(path_.replace("png",'avif'),'AVIF')
